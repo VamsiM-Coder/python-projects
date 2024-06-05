a63dd33fb5009db64cd27be7c16ba1dd9305076a
JASHWANTH-AKULA77 <148619775+JASHWANTH-AKULA77@users.noreply.github.com>
2024-06-05 12:56:18 +0530

    Create Linear Regression.py

---
+++ Linear Regression.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+
+
+ 
+X = data
+y = target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+model = LinearRegression()
+
+
+model.fit(X_train, y_train)
+
+
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+train_score = model.score(X_train, y_train)
+test_score = model.score(X_test, y_test)
+
+print("Training score:", train_score)
+print("Testing score:", test_score)
+
+# Plot residuals
+plt.scatter(y_train_pred, y_train_pred - y_train, c='blue', marker='o', label='Training data')
+plt.scatter(y_test_pred, y_test_pred - y_test, c='lightgreen', marker='s', label='Testing data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=0, xmax=50, lw=2, color='red')
+plt.title('Residual plot')
+plt.show()
